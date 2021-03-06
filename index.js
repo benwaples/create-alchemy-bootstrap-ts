62445c9b279f4e24e3b6e928b3f27815655dce55
benwaples <benwaples@gmail.com>
2021-03-05 18:15:45 -0800

    update where dependencies get installed

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ if(folder !== '.') {
 
 fs.copy(`${__dirname}/copyables`, folder, err => {
   if(err) console.log('something went wrong')
-  if(folder !== '.') return console.log(`\n ${folder} is ready for coding \n`)
-  if(folder === '.') return console.log('\n project files have been placed in this directory \n')
+  if(folder !== '.') {
+    console.log(`\n ${folder} is ready, now installing dependencies \n`)
+    child_process.exec(`cd ${folder} && npm i`)
+    return console.log('\n dependencies are installed \n')
+  }
+  if(folder === '.') {
+    console.log('\n project files have been placed in this directory, installing dependencies \n')
+    child_process.exec('npm i')
+    return console.log('\n dependencies are installed \n')
+  }
   console.log('\n Happy Coding! \n')
 })
 
-child_process.exec("npm i", (error, stdout, stderr) => {
-  if(error) console.log('error while installing dependencies')
-})
