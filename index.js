6d1ce0f1f2cd8e2e3174a9113b7fb8ff1f511d08
benwaples <benwaples@gmail.com>
2021-03-05 18:10:09 -0800

    install dependencies

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const fs = require('fs-extra')
 const path = require('path')
 const child_process = require('child_process')
 
-function makeProjectFolder(folder) {return folder !== '.'}
 
-const args = process.args;
-const folder = ars[args.length - 1];
+const args = process.argv;
+const folder = args[args.length - 1];
 
-if(makeProjectFolder(folder)) {
+if(folder !== '.') {
   fs.mkdirSync(folder);
 }
 
 fs.copy(`${__dirname}/copyables`, folder, err => {
   if(err) console.log('something went wrong')
-  if(makeProjectFolder(folder)) return console.log(`\n ${folder} is ready for coding \n`)
-  if(!makeProjectFolder(folder)) return console.log('\n project files have been placed in this directory \n')
+  if(folder !== '.') return console.log(`\n ${folder} is ready for coding \n`)
+  if(folder === '.') return console.log('\n project files have been placed in this directory \n')
+  console.log('\n Happy Coding! \n')
+})
+
+child_process.exec("npm i", (error, stdout, stderr) => {
+  if(error) console.log('error while installing dependencies')
 })
